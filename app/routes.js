3fe5ef589b622e5fc3727c39105911d7afe43679
haarry201 <harrymkenny@gmail.com>
2024-09-17 15:48:11 +0100

    Update routes.js
    Changed routing for laptop page in order to store device type in laptopType variable

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+router.post('/uses-laptop', function (req, res) {
+
+    var doYouHaveLaptop = req.session.data['doYouHaveLaptop']
+
+    if (doYouHaveLaptop == "yes"){
+        var laptopType = req.session.data['laptopType']
+    }
+    res.redirect('/devices/desktop')
+    
+    
+  
+  })
