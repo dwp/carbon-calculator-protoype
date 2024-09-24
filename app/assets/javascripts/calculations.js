88fd93285aa27418c10a9c906e9b7c271a7bfefe
haarry201 <harrymkenny@gmail.com>
2024-09-24 16:06:55 +0100

    Updated comments and deleted old code

--- app/assets/javascripts/calculations.js
+++ app/assets/javascripts/calculations.js
@@ -2,32 +2,15 @@
 let factors;
 
 
-// var nunjucks  = require('nunjucks');
-// var env = nunjucks.configure();
-// env.addGlobal('calculateLaptopCarbon', calculateLaptopCarbon);
-
-
-// async function fetchFactors() {
-//         try{
-//             const url = "/public/jsons/factors.json";
-//             const response = await fetch(url);
-//             factors = await response.json();
-//             if (!response.ok) throw new Error('Network response was not ok: ${response.status}');
-//             return factors;
-//         }
-//         catch (error){
-//             alert(error.message, error);
-//             throw error;
-//         }
-//  }
-
-
 async function fetchFactors() {
     try {
+        //wait for factors.json file to be fetched
         const response = await fetch('/public/jsons/factors.json');
+        //if there is an issue with connection to factors file, throw error
         if (!response.ok) throw new Error('Network response was not ok');
         return await response.json();
     } catch (error) {
+        //display console error and alert if issues with fetching json file
         console.error('Failed to fetch factors:', error);
         alert('Failed to fetch carbon factors. Please try again later.');
         throw error;
@@ -38,27 +21,24 @@ async function fetchFactors() {
 
 async function calculateLaptopCarbon(event){
     event.preventDefault();
-    
+    //try to fetch factors.json file by calling to fetchFactors function
     try {
         factors = await fetchFactors();
     } catch {
         return;
     }
 
-    //var laptop = JSON.stringify(factors.deviceFactors.Laptop['embodied']);
-    //var result = laptop*3;
-
+    //Get laptop type from current HTML page
     var laptopType = document.getElementById("data-holder").textContent;
+    //find the laptop emissions based on laptop type
     var laptopStats = factors.deviceFactors.Laptop[laptopType];
+    //get laptop embodied and usage emissions and convert to strings
     var laptopEmbodied = JSON.stringify(laptopStats['embodied']);
     var laptopUsage = JSON.stringify(laptopStats['usagePerYear']);
-
+    //calculate total laptop emissions
     var laptopTotal = +laptopEmbodied + (+laptopUsage * 5);
 
-
-    //const laptopFactors = getLaptopText();
-    //const sampleText = data['laptop-type'];
-
+    //change html element to value for total laptop emissions
     document.getElementById("changeMe").textContent=laptopTotal;
 }
 
