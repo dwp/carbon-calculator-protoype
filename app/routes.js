69b28e36ca988ffb5183d17eb64850a2d98e755d
haarry201 <harrymkenny@gmail.com>
2024-09-25 10:37:21 +0100

    Deleted routes as currently unneeded

--- app/routes.js
+++ app/routes.js
@@ -8,41 +8,4 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-var laptopType
-var smartphoneType
-var numberOfMonitors
 
-
-router.post('/uses-laptop', function (req, res) {
-
-    var doYouHaveLaptop = req.session.data['doYouHaveLaptop']
-
-    if (doYouHaveLaptop == "yes"){
-        laptopType = req.session.data['laptopType']
-    }
-    res.redirect('/devices/deviceSummary')
-
-  })
-
-  router.post('/uses-smartphone', function (req, res) {
-
-    var doYouHaveSmartphone = req.session.data['doYouHaveSmartphone']
-
-    if (doYouHaveSmartphone == "yes"){
-        smartphoneType = req.session.data['smartphoneType']
-    }
-    res.redirect('/devices')
-    
-  })
-
-  router.post('/uses-monitors', function (req, res) {
-
-    var doYouHaveMonitor = req.session.data['doYouHaveMonitor']
-
-    if (doYouHaveMonitor == "yes"){
-        numberOfMonitors = req.session.data['numberOfMonitors']
-    }
-
-    res.redirect('/devices/smartphone')
-    
-  })
