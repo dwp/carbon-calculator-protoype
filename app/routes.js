6ce649aea571277912fe0b96b24ac1525d3ea55d
haarry201 <harrymkenny@gmail.com>
2024-10-10 10:33:26 +0100

    Added routing to car type page

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,12 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
-
+router.post('/check-car-type', function (req, res){
+    var transportType = req.session.data['commuteType']
+    if (transportType == 'car'){
+        res.redirect('/travel/car-type')
+    } else {
+        res.redirect('/travel/business-travel')
+    }
+})
 
