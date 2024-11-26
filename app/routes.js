ecfd7fb04fef17b5b105c7fa1f699b767cc7e1c5
haarry201 <harrymkenny@gmail.com>
2024-11-26 12:19:35 +0000

    Commuting/business travel overhaul
    Commuting updated to include option for different return journey. Business travel updated to include option for different car type to commute

--- app/routes.js
+++ app/routes.js
@@ -17,12 +17,32 @@ const router = govukPrototypeKit.requests.setupRouter()
 // })
 
 router.post('/check-car-type', function(req, res){
-    var bTravelTransportType = req.session.data['businessTravelMode']
-    var commuteTransportType = req.session.data['commuteType']
-    if (bTravelTransportType == 'car' || commuteTransportType == 'car'){
-        res.redirect('/travel/car-type')
+    var bTravelTransportType = req.session.data['businessTravelMode'];
+    if (bTravelTransportType == 'car'){
+        res.redirect('/travel/business-travel-car-type');
     } else {
-        res.redirect('/summary')
+        res.redirect('/summary');
     }
 })
+
+router.post('/check-commute-type', function(req, res){
+    var commuteMode = req.session.data['commuteType'];
+    if (commuteMode == 'car'){
+        res.redirect('/travel/commute-car');
+    } else {
+        res.redirect('/travel/return-commute');
+    }
+})
+
+router.post('/check-return', function(req, res){
+    var sameReturn = req.session.data['same-return']
+    if (sameReturn == 'no'){
+        res.redirect('/travel/return-commute/return-mode');
+    }
+    else {
+        res.redirect('/travel/commute-distance');
+    }
+})
+
+
 
